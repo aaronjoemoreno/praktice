a60900b77481bd14520c64bf0a222a0d6648f4cf
Aaron Moreno <aaron.j.moreno@gmail.com>
2021-01-12 23:02:27 -0700

    added temp about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import Header from '../components/header'
+import Logo from '../components/logo'
 
 const Container = styled.div`
     height: 100vh;
     width: 100vw;
+    text-align: center;
 `
 
 const About = () => {
     return (
         <Container>
-           <h1>ABOUT</h1>
+            <Header />
+            <Logo />
+           <h1>ABOUT Us Coming Soon...</h1>
         </Container>
     )
 }
