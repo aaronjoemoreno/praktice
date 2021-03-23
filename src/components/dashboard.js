90ec1477e4d23d90571a1f7785a81faa80f3c9c5
Aaron Moreno <aaron.j.moreno@gmail.com>
2021-03-22 23:37:10 -0600

    removed for temp nav build out

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import MemberShipNav from './membership/membershipNav'
-import Logo from './logo'
-import CertificationAndInsurance from './membership/certificationAndInsurance'
-import "normalize.css"
-import './layout.css'
+// import React, { useState } from 'react'
+// import styled from 'styled-components'
+// import MemberShipNav from './membership/membershipNav'
+// import Logo from './logo'
+// import CertificationAndInsurance from './membership/certificationAndInsurance'
+// import "normalize.css"
+// import './layout.css'
 
-const Container = styled.div`
-    width: 100%;
-    text-align: center;
-`
+// const Container = styled.div`
+//     width: 100%;
+//     text-align: center;
+// `
 
-const Dashboard = () => {
-    //Move to useContext
-    // const [verified, setVerified] = useState(false);
-    const [status, setStatus] = useState(1);
+// const Dashboard = () => {
+//     //Move to useContext
+//     // const [verified, setVerified] = useState(false);
+//     const [status, setStatus] = useState(1);
 
-    return (
-        <Container>
-            <MemberShipNav />
-            <Logo />
-            <h1>Welcome!</h1>
+//     return (
+//         <Container>
+//             <MemberShipNav />
+//             <Logo />
+//             <h1>Welcome!</h1>
 
-            {status === 1 &&
-                <CertificationAndInsurance />
-            }
-        </Container>
-    )
-}
+//             {status === 1 &&
+//                 <CertificationAndInsurance />
+//             }
+//         </Container>
+//     )
+// }
 
-export default Dashboard
+// export default Dashboard
