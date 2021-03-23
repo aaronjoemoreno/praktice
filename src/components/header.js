af1fd6f98beea341da1c07fad6ddbfacd1d2ab2b
Aaron Moreno <aaron.j.moreno@gmail.com>
2021-03-22 22:52:06 -0600

    reverted for only nav

--- src/components/header.js
+++ src/components/header.js
@@ -25,7 +25,7 @@ const Container = styled.div`
   a{
     font-size: 1.5rem;
     color: var(--main-color);
-    list-style: none;
+    text-decoration: none;
   }
 
   a:nth-child(3){
@@ -34,58 +34,19 @@ const Container = styled.div`
 
   a:hover{
     text-decoration: underline var(--secondary-color);
+    text-decoration-line: underline;
+    text-decoration-color: var(--secondary-color);
   }
 
-  .dropdown {
-    position: relative;
-    display: inline-block;
-    text-align: center;
-  }
-
-  .dropdown-content {
-    display: none;
+  .dropdown{
     position: absolute;
-    left: 0;
-    right: 0;
-    top: 4.5rem;
-    background-color: #f1f1f1;
-    box-shadow: 0px 3px 15px rgba(0,0,0,0.4);
-    z-index: 1;
-    border-radius: 2rem;
-  }
-  .dropdown-content ul{
-    padding: 0;
-  }
+    top: 50px;
+    width: 200px;
+    background: white;
 
-  .dropdown-content ul li{
-    color: black;
-    padding: 1rem;
-    list-style: none;
-    display: block;
-    text-align: center;
-  }
-
-  .dropdown-content ul li a{
-    width: 100%;
-    margin: 0 auto;
-  }
-
-  .dropdown-content li a:hover {
-    text-decoration: underline var(--secondary-color) !important;
-    text-decoration-line: underline !important;
-    text-decoration-color: var(--secondary-color) !important;
-  }
-
-  .dropdown:hover .dropdown-content {display: block;}
-
-  .dropdown:hover .dropbtn {background-color: #3e8e41;}
-
-  #praktice-nav{
-    font-size: 1.8rem;
-  }
-
-  #praktice-nav:hover .dropdown {
-    display: block;
+    ul{
+      list-style: none;
+    }
   }
 
 
@@ -123,25 +84,6 @@ const Container = styled.div`
       padding: 0;
       margin: 0 auto;
       margin-top: 2rem;
-
-      &:hover{
-        cursor: pointer;
-      }
-    }
-
-    .mobile-nav-expand{
-      text-align: center;
-
-      ul{
-        margin: 0 auto;
-        padding-inline-start: 0;
-      }
-
-      ul li{
-        list-style-type: none;
-        margin-top: .5rem;
-        text-decoration: underline var(--secondary-color) !important;
-      }
     }
   }
 `
@@ -149,13 +91,10 @@ const Container = styled.div`
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [navState, setNavState] = useState('')
-  const [aboutIsOpen, setAboutIsOpen] = useState(false);
-  const [prakticeIsOpen, setPrakticeIsOpen] = useState(false);
+  const [display, setDisplay] = useState(false);
 
   useEffect(() =>{
-    isOpen ? setNavState('open') : setNavState('');
-    navState == ('') ? setAboutIsOpen(false) : setAboutIsOpen(aboutIsOpen);
-    navState == ('') ? setPrakticeIsOpen(false) : setPrakticeIsOpen(prakticeIsOpen);
+    isOpen ? setNavState('open') : setNavState('')
   }, [isOpen])
 
 
@@ -166,11 +105,11 @@ const Header = () => {
         <h4>HOME</h4>
       </Link>
 
-      <div class="dropdown">
-        <Link to="/about" className="dropbtn">
-          <h4>ABOUT</h4>
-        </Link>
-        <div className="dropdown-content">
+      <Link to="/about">
+        <h4 onMouseOver={() => setDisplay(true)} onClick={() => display ? setDisplay(false): setDisplay(true)}>ABOUT</h4>
+        {/* <h4>ABOUT</h4> */}
+        {display &&
+        <div className="dropdown">
           <ul>
             <li>
               <Link to="/about/#covid">Covid 19</Link>
@@ -183,13 +122,14 @@ const Header = () => {
             </li>
           </ul>
         </div>
-      </div>
+      }
+      </Link>
 
-      <div className="dropdown">
-        <Link to="/praktices" className="dropbtn">
-        <h4 id="praktice-nav">PRA<span style={{color: `var(--secondary-color)`}}>K</span>TICES</h4>
-        </Link>
-        <div className="dropdown-content">
+      <Link to="/praktices">
+      <h4 onMouseOver={() => setDisplay(true)} onClick={() => display ? setDisplay(false): setDisplay(true)}>PRA<span style={{color: `var(--secondary-color)`}}>K</span>TICES</h4>
+      {/* <h4>PRA<span style={{color: `var(--secondary-color)`}}>K</span>TICES</h4> */}
+        {display &&
+        <div className="dropdown">
           <ul>
             <li>
               <Link to="/praktices/#application">Application Process</Link>
@@ -205,7 +145,8 @@ const Header = () => {
             </li>
           </ul>
         </div>
-      </div>
+      }
+      </Link>
 
       <Link to="/contact">
         <h4>CONTACT</h4>
@@ -230,46 +171,13 @@ const Header = () => {
         <h4>HOME</h4>
       </Link>
 
-      <>
-        <a><h4 onClick={() => setAboutIsOpen(!aboutIsOpen)}>ABOUT</h4></a>
-        {aboutIsOpen &&
-        <div className="mobile-nav-expand">
-          <ul>
-            <li>
-              <Link to="/about/#covid">Covid 19</Link>
-            </li>
-            <li>
-              <Link to="/about/#values">Our Values</Link>
-            </li>
-            <li>
-              <Link to="/about/#mission">Our Mission</Link>
-            </li>
-          </ul>
-        </div>
-      }
-      </>
+      <Link to="/about">
+        <h4>ABOUT</h4>
+      </Link>
 
-      <>
-      <a><h4 onClick={() => setPrakticeIsOpen(!prakticeIsOpen)}>PRA<span style={{color: `var(--secondary-color)`}}>K</span>TICES</h4></a>
-      {prakticeIsOpen &&
-        <div className="mobile-nav-expand">
-          <ul>
-            <li>
-              <Link to="/praktices/#application">Application Process</Link>
-            </li>
-            <li>
-              <Link to="/praktices/#rates">Rates and Prices</Link>
-            </li>
-            <li>
-              <Link to="/praktices/#cancellation">Cancellation Policy</Link>
-            </li>
-            <li>
-              <Link to="/praktices/#gallery">Gallery</Link>
-            </li>
-          </ul>
-        </div>
-      }
-      </>
+      <Link to="/praktices">
+        <h4>PRA<span style={{color: `var(--secondary-color)`}}>K</span>TICES</h4>
+      </Link>
 
       <Link to="/contact">
         <h4>CONTACT</h4>
