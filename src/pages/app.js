cf325c322c49776fa29a3737e05897053dc2e64b
Aaron Moreno <aaron.j.moreno@gmail.com>
2021-03-22 22:42:28 -0600

    added membership page and auth

--- src/pages/app.js
+++ src/pages/app.js
@@ -7,10 +7,12 @@ import About from './about'
 import Praktices from './praktices'
 import Contact from './contact'
 import Members from './members'
+import Admin from './admin'
 
 const App = () => (
     <Router>
       <PrivateRoute path="/dashboard" component={Dashboard} />
+      <PrivateRoute path="/admin" component={Admin} />
       <IndexPage path="/" />
       <About path="/about"/>
       <Praktices path="/praktices" />
